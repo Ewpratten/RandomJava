5ca6316a6ca5b775e6a59b90e103abe63d1d77d6
Evan Pratten <ewpratten@gmail.com>
2019-02-08 13:00:07 -0500

    stick figure

---
+++ StickFigure.java
@@ -0,0 +1,19 @@
+/*
+Evan Pratten
+Feb 8 2019
+Learning print and escapes
+*/
+
+public class StickFigure{
+  public static void main(String[] args){
+    System.out.println(" \\(\")/");
+    System.out.println("  ==");
+    System.out.println("  ||");
+    System.out.println("  /\\");
+    System.out.println("_/  \\_");
+    
+    // In a single line
+    System.out.print("\n\n  \\(\")/\n   ==\n   ||\n   /\\\n _/  \\_\n");
+    return;
+  }
+}
